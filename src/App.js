37ef546457f752f15f087b5f7253ad3ac6a58de7
akshayakula <akshayakula@vt.edu>
2022-04-01 16:02:21 -0400

    base kelpr

--- src/App.js
+++ src/App.js
@@ -1,33 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
 import { SecretNetworkClient } from "secretjs";
+import {useState} from 'react';
 
-async function App() {
+function App() {
 
-  const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+  let [secret, setSecret] = useState({})
 
-  while (
-    !window.keplr ||
-    !window.getEnigmaUtils ||
-    !window.getOfflineSignerOnlyAmino
-  ){
-    await sleep(100);
+  let test = async() => {
+
+    console.log("PRESED!")
+    // event.preventdefault()
+    const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+    while (
+      !window.keplr ||
+      !window.getEnigmaUtils ||
+      !window.getOfflineSignerOnlyAmino
+    ){
+      await sleep(100);
+    }
+    
+    const CHAIN_ID = "pulsar-2";
+    
+    await window.keplr.enable(CHAIN_ID);
+    
+    const keplrOfflineSigner = window.getOfflineSignerOnlyAmino(CHAIN_ID);
+    const [{ address: myAddress }] = await keplrOfflineSigner.getAccounts();
+    
+    const secretjs = await SecretNetworkClient.create({
+      grpcWebUrl: "http://rpc.pulsar.griptapejs.com:9091",
+      chainId: CHAIN_ID,
+      wallet: keplrOfflineSigner,
+      walletAddress: myAddress,
+      encryptionUtils: window.getEnigmaUtils(CHAIN_ID),
+    });
+    console.log({address : secretjs})
+    setSecret({address : secretjs})
+
   }
-  
-  const CHAIN_ID = "secret-4";
-  
-  await window.keplr.enable(CHAIN_ID);
-  
-  const keplrOfflineSigner = window.getOfflineSignerOnlyAmino(CHAIN_ID);
-  const [{ address: myAddress }] = await keplrOfflineSigner.getAccounts();
-  
-  const secretjs = await SecretNetworkClient.create({
-    grpcWebUrl: "https://grpc-web.azure-api.net",
-    chainId: CHAIN_ID,
-    wallet: keplrOfflineSigner,
-    walletAddress: myAddress,
-    encryptionUtils: window.getEnigmaUtils(CHAIN_ID),
-  });
+
   
   // Note: Using `window.getEnigmaUtils` is optional, it will allow
   // Keplr to use the same encryption seed across sessions for the account.
@@ -38,8 +50,11 @@ async function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={() => {test()}}> 
+          test
+        </button>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {secret.walletAddress}
         </p>
         <a
           className="App-link"
